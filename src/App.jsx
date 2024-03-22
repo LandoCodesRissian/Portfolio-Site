59dfae4a4de5971777633522d556f3561648fd1d
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-03-21 18:45:26 -0600

    fixed files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks,
+  Hero, Navbar, Tech, Works, StarsCanvas} from 
+  './components';
+
 const App = () => {
   return (
-    <div>
-      3D Developer Portfolio
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+
     </div>
+    </BrowserRouter>
   )
 }
 
