e7655ba0c04852e6d5fbee3721efa9d2f57e09bc
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-03-26 17:53:12 -0600

    added a lot to the work component still have more to add though

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,21 +4,16 @@ import {
     creator,
     web,
     javascript,
-    typescript,
     html,
     css,
     reactjs,
-    redux,
     tailwind,
     nodejs,
     mongodb,
     git,
     figma,
-    docker,
     UofU,
-    carrent,
-    jobit,
-    tripguide,
+    giftGenie,
     threejs,
     Freelance,
   } from "../assets";
@@ -159,9 +154,9 @@ import {
   
   const projects = [
     {
-      name: "Car Rent",
+      name: "Gift Genie",
       description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        "A website that is built to help a user find a gift for someone, allows them to save items and link to purchase said items",
       tags: [
         {
           name: "react",
@@ -176,50 +171,9 @@ import {
           color: "pink-text-gradient",
         },
       ],
-      image: carrent,
-      source_code_link: "https://github.com/",
-    },
-    {
-      name: "Job IT",
-      description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "restapi",
-          color: "green-text-gradient",
-        },
-        {
-          name: "scss",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: jobit,
-      source_code_link: "https://github.com/",
-    },
-    {
-      name: "Trip Guide",
-      description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-      tags: [
-        {
-          name: "nextjs",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "supabase",
-          color: "green-text-gradient",
-        },
-        {
-          name: "css",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: tripguide,
-      source_code_link: "https://github.com/",
+      image: giftGenie,
+      source_code_link: "https://github.com/LandoCodesRissian/Gift-Genie?tab=readme-ov-file",
+      live_link: "https://giftgenie-vfsj.onrender.com"
     },
   ];
   
